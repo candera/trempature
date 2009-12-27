4249b2df5bf64c353bb83d7d7cef3aa0c9188f90
unknown <candera@.(none)>
2009-12-27 08:32:16 -0600

    Initial commit

---
+++ trempature/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace trempature
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 120000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "Trempature";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(105, 48);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showToolStripMenuItem.Text = "&Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ trempature/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Xml.Linq;
+
+namespace trempature
+{
+    public partial class Form1 : Form
+    {
+        private bool _allowClose;
+        private Font _iconFont; 
+        private string _tempText = "XX";
+        private DateTime _lastUpdate = DateTime.MinValue; 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _iconFont = new Font(FontFamily.GenericSansSerif, 20, FontStyle.Regular); 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            RetrieveTemperatureAsync();
+        }
+
+        private void RetrieveTemperatureAsync()
+        {
+            ThreadPool.QueueUserWorkItem(RetrieveTemperature);
+        }
+
+        private void RetrieveTemperature(object state)
+        {
+            try
+            {
+                var doc = XDocument.Load("http://www.weather.gov/xml/current_obs/KMSP.xml");
+                var tempValue = doc.Root.Element("temp_f").Value;
+                float temp = float.Parse(tempValue);
+                _tempText = ((int)(temp + 0.5F)).ToString();
+                _lastUpdate = DateTime.Now;
+                Invoke(new Action(UpdateIcon)); 
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void UpdateIcon()
+        {
+            notifyIcon1.Icon = GetIcon();
+        }
+
+        private void DrawFilledRegion(Graphics g, Pen pen, Brush brush, int x, int y, int width, int height)
+        {
+            g.FillRectangle(brush, x, y, width, height);
+            g.DrawRectangle(pen, x, y, width, height);
+        }
+        private Icon GetIcon()
+        {
+            Bitmap bitmap = new Bitmap(32, 32);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            g.Clear(Color.Transparent);
+            g.DrawRectangle(Pens.Black, new Rectangle(1, 1, 30, 30));
+
+            Brush background;
+
+            if ((DateTime.Now - _lastUpdate) > TimeSpan.FromMinutes(30))
+            {
+                background = Brushes.DarkRed;
+            }
+            else
+            {
+                background = Brushes.Black; 
+            }
+
+            g.FillRectangle(background, new Rectangle(2, 2, 28, 28));
+
+            var centerPoint = GetCenterPoint(g, _tempText, _iconFont, 32, 32); 
+
+            g.DrawString(_tempText, _iconFont, Brushes.Yellow, centerPoint); 
+
+            //DrawFilledRegion(g, Pens.Black, Brushes.Yellow, 13, 13, 8, 8);
+
+            return Icon.FromHandle(bitmap.GetHicon());
+        }
+
+        private PointF GetCenterPoint(Graphics g, string s, Font font, int width, int height)
+        {
+            var size = g.MeasureString(s, font);
+
+            return new PointF((width / 2) - (size.Width / 2) + 0.5F, (height / 2) - (size.Height / 2) + 0.5F); 
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (!_allowClose)
+            {
+                PutToTray();
+                e.Cancel = true;
+            }
+            base.OnClosing(e);
+        }
+        private void PutToTray()
+        {
+            Hide();
+            ShowInTaskbar = false;
+        }
+        private void RestoreFromTray()
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            ShowInTaskbar = true;
+            Activate();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            RestoreFromTray();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateIcon(); 
+            PutToTray();
+            RetrieveTemperatureAsync(); 
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RestoreFromTray(); 
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _allowClose = true;
+            Close(); 
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            RestoreFromTray(); 
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                PutToTray();
+            }
+        }
+
+    }
+}
